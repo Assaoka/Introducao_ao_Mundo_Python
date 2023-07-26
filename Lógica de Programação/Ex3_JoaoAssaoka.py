cec16490fbcc3121217798a5e3a3f9a19b5b0203
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-26 19:24:41 -0300

    Create Ex3_JoaoAssaoka.py

---
+++ Lógica de Programação/Ex3_JoaoAssaoka.py
@@ -0,0 +1,8 @@
+# 3-) Leia um valor inteiro e apresente os resultados do quadrado, do cubo e da raiz quadrada do valor lido.
+
+N = int(input("Digite um Valor Inteiro: "))
+print(30 * "=")
+print(f"{N}^2 = {N ** 2}")
+print(f"{N}^3 = {N ** 3}")
+print(f"√{N} = {N ** (1/2)}")
+print(30 * "=")
