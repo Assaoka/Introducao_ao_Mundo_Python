f1d78a56214596dad2c9db7bedca050ede7890bd
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-28 22:25:16 -0300

    Create Desafio_007_JoaoAssaoka.py

---
+++ Curso em Vídeo (Python 3)/Desafio_007_JoaoAssaoka.py
@@ -0,0 +1,6 @@
+# DESAFIO 007 - Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+Nota_1 = float(input('Nota 1: '))
+Nota_2 = float(input('Nota 2: '))
+
+print(f'Média: {(Nota_1 + Nota_2)/2}')
