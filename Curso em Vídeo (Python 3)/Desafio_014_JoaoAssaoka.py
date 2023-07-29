99a4f19e30824afe95267de2c523c245d9686125
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-28 23:22:17 -0300

    Create Desafio_014_JoaoAssaoka.py

---
+++ Curso em Vídeo (Python 3)/Desafio_014_JoaoAssaoka.py
@@ -0,0 +1,6 @@
+# DESAFIO 014 - Escreva um programa que converta uma temperatura em graus Celsius para graus Fahrenheit.
+
+Tc = float(input('Digite uma Temperatura (C°): '))
+Tf = 1.8 * Tc + 32
+
+print(f'{Tc} C° = {Tf} F°')
