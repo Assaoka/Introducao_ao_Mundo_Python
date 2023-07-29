8eac60196ad38db45e8185cbb73f293df518c0fa
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-28 22:39:04 -0300

    Create Desafio_009_JoaoAssaoka.py

---
+++ Curso em Vídeo (Python 3)/Desafio_009_JoaoAssaoka.py
@@ -0,0 +1,14 @@
+# DESAFIO 009 - Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+
+N = int(input('Digite um número inteiro: '))
+
+print(f'{N} * 1 = {N * 1}')
+print(f'{N} * 2 = {N * 2}')
+print(f'{N} * 3 = {N * 3}')
+print(f'{N} * 4 = {N * 4}')
+print(f'{N} * 5 = {N * 5}')
+print(f'{N} * 6 = {N * 6}')
+print(f'{N} * 7 = {N * 7}')
+print(f'{N} * 8 = {N * 8}')
+print(f'{N} * 9 = {N * 9}')
+print(f'{N} * 10 = {N * 10}')
