7b6f6292cd09e1518d2daaee53120e42b0ea92f3
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-29 20:49:20 -0300

    Update Desafio_015_JoaoAssaoka.py

--- Curso em Vídeo (Python 3)/Desafio_015_JoaoAssaoka.py
+++ Curso em Vídeo (Python 3)/Desafio_015_JoaoAssaoka.py
@@ -1,4 +1,4 @@
-# DESAFIO 015 -Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
+# DESAFIO 015 - Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
 
 Q_km = float(input('Qual a quantidade de km percorridos: '))
 Q_Dias = int(input('Por quanto dias o carro foi alugado: '))
