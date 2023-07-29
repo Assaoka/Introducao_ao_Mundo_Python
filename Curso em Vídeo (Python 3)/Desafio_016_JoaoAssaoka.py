e6c1c790922551cab587c976884f83d70b9490c2
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-29 13:31:02 -0300

    Create Desafio_016_JoaoAssaoka.py

---
+++ Curso em Vídeo (Python 3)/Desafio_016_JoaoAssaoka.py
@@ -0,0 +1,5 @@
+# DESAFIO 016 - Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+
+N = float(input('Digite um número real: '))
+
+print(f'A parte inteira de `{N} é {int(N)}')
