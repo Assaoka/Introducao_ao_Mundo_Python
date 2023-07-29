c6fe5b91e5ed42853666810cf8c342b44fc0dd05
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-28 23:24:17 -0300

    Update Desafio_002_JoaoAssaoka.py

--- Curso em Vídeo (Python 3)/Desafio_002_JoaoAssaoka.py
+++ Curso em Vídeo (Python 3)/Desafio_002_JoaoAssaoka.py
@@ -1,4 +1,4 @@
-# DESAFIO 002 - Faça um programa qua leia o nome de uma passoa e mostre uma mansagam de boas—vindas.
+# DESAFIO 002 - Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 
 Nome = input("Qual o seu nome? ")
 print(f'É um prazer te conhecer {Nome}!')
