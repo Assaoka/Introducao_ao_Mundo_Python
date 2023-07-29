071d969d30ee46f41881f78385596de3f3940e27
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-28 23:10:03 -0300

    Create Desafio_012_JoaoAssaoka.py

---
+++ Curso em Vídeo (Python 3)/Desafio_012_JoaoAssaoka.py
@@ -0,0 +1,6 @@
+# DESAFIO 012 - Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+Preço = float(input('Preço: R$ '))
+Preço *= 0.95 
+
+print(f'O novo preço é {Preço:.2f}')
