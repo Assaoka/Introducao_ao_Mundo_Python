848fd1ad6ab6bfa9e4c2d63f725c737daa209e45
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-28 22:18:14 -0300

    Create Desafio_005_JoaoAssaoka.py

---
+++ Curso em Vídeo (Python 3)/Desafio_005_JoaoAssaoka.py
@@ -0,0 +1,6 @@
+# DESAFIO 005 - Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+N = int(input('Digite um número: '))
+
+print(f'Sucessor: {N + 1}')
+print(f'Antecessor: {N - 1}')
