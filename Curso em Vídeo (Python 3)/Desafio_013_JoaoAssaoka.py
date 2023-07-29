922e0e4f972f3ae0bac6792b280e9ba5c10847bc
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-28 23:15:25 -0300

    Create Desafio_013_JoaoAssaoka.py

---
+++ Curso em Vídeo (Python 3)/Desafio_013_JoaoAssaoka.py
@@ -0,0 +1,6 @@
+# DESAFIO 013 - Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+Salário = float(input('Salário: R$ '))
+Salário *= 1.15
+
+print(f'O Salário após o reajuste é {Salário:.2f}')
