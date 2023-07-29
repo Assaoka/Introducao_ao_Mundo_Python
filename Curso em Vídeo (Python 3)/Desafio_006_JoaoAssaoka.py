bdb9cdde09cf9cfc6f3d15d81d2964647dacf6c0
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-28 22:22:30 -0300

    Create Desafio_006_JoaoAssaoka.py

---
+++ Curso em Vídeo (Python 3)/Desafio_006_JoaoAssaoka.py
@@ -0,0 +1,7 @@
+# DESAFIO 006 - Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+N = float(input('Digite um Número: '))
+
+print(f'{N} * 2 = {N * 2}')
+print(f'{N} * 3 = {N * 3}')
+print(f'√{N} = {N**(1/2)}')
