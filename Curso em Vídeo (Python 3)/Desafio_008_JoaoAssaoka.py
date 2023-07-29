766cde389eb3010df2d95f540163f7d6d8d8d407
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-28 22:31:18 -0300

    Create Desafio_008_JoaoAssaoka.py

---
+++ Curso em Vídeo (Python 3)/Desafio_008_JoaoAssaoka.py
@@ -0,0 +1,6 @@
+# DESAFIO 008 - Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+Valor = float(input('Digite o Valor em Metros: '))
+
+print(f'Centímetros: {Valor*100}')
+print(f'Milímetros: {Valor*1000}')
