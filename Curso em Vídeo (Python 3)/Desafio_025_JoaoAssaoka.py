41eedaf70779c0e5c89ade722b418bee8d9defa7
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-07-29 21:25:35 -0300

    Create Desafio_025_JoaoAssaoka.py

---
+++ Curso em Vídeo (Python 3)/Desafio_025_JoaoAssaoka.py
@@ -0,0 +1,5 @@
+# DESAFIO 025 - Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+
+Nome = input('Qual o Seu Nome: ').upper()
+
+print(f'O nome possui SILVA? {"SILVA" in Nome}')
