5a67b4192d991a6c669c7514db26b0960a5da810
João Assaoka <130188340+Assaoka@users.noreply.github.com>
2023-08-19 19:25:20 -0300

    Create Desafio_060_For_JoaoAssaoka.py

---
+++ Curso em Vídeo (Python 3)/Mundo 2 [40 Horas]/Desafio_060_For_JoaoAssaoka.py
@@ -0,0 +1,18 @@
+# DESAFIO 060 - Faça um programa que leia um número qualquer e mostre o seu fatorial. Exemplo: 5! = 5 x 4 x 3 x 2 x 1 = 120
+# Usando For
+
+# Formatações de texto para o terminal:
+NEGRITO = '\033[1m' # 1 = Negrito
+VERMELHO = '\033[1;31m' # 1 = Negrito, 31 = Letra Vermelha
+FIM = '\033[m' # Fim da formatação.
+
+n = int(input(f'\n{NEGRITO}Digite um Número Inteiro Positivo:{FIM} '))
+while n < 0:
+    n = int(input(f'{NEGRITO}Digite um Número Inteiro {VERMELHO}Positivo:{FIM} '))
+
+print(f'{NEGRITO}{n}! = ', end='')
+fatorial = 1
+for n in range(n, 1, -1):
+    print(f'{n}', end=' * ')
+    fatorial *= n
+print(f'1 = {fatorial}{FIM}\n')
